c2315bc2039cd1b65a2a1c3d30783c0ae9b83306
chsakell <chsakell@gmail.com>
2018-10-29 20:27:13 +0200

    add table and blob storage output

---
+++ ServerlessApp/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessApp.Models
+{
+    public class Order : Transaction
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+    }
+}
--- ServerlessApp/ProcessSuccessCharge.cs
+++ ServerlessApp/ProcessSuccessCharge.cs
@@ -1,15 +1,41 @@
 using System;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using ServerlessApp.Models;
 
 namespace ServerlessApp
 {
     public static class ProcessSuccessCharge
     {
         [FunctionName("ProcessSuccessCharge")]
-        public static void Run([QueueTrigger("success-charges", Connection = "")]string myQueueItem, TraceWriter log)
+        public static void Run([QueueTrigger("success-charges", Connection = "")]
+        Transaction transaction,
+        [Blob("licences/{rand-guid}.lic")] TextWriter licence,
+        [Table("orders")] out Order order,
+        TraceWriter log)
         {
-            log.Info($"C# Queue trigger function processed: {myQueueItem}");
+            log.Info($"C# Queue trigger function processed: {transaction}");
+
+            order = new Order
+            {
+                PartitionKey = "stripe",
+                RowKey = transaction.Id,
+                Amount = transaction.Amount,
+                CardType = transaction.CardType,
+                Currency = transaction.Currency,
+                CustomerEmail = transaction.CustomerEmail,
+                CustomerId = transaction.CustomerId,
+                CustomerName = transaction.CustomerName,
+                Id = transaction.Id,
+                DateCreated = transaction.DateCreated,
+                StripeCustomerId = transaction.StripeCustomerId
+            };
+
+            licence.WriteLine($"Transaction ID: {transaction.Id}");
+            licence.WriteLine($"Email: {transaction.CustomerEmail}");
+            licence.WriteLine($"Amount payed: {transaction.Amount}  {transaction.Currency}");
+            licence.WriteLine($"Licence key: {Guid.NewGuid().ToString()}");
         }
     }
 }
